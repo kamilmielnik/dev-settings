76050c4120d1a83c0b56a9c43a8d286ce6e05196
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-05-10 02:04:41 +0700

    Use * for gzip_types

--- vps/nginx/enable-gzip-types.js
+++ vps/nginx/enable-gzip-types.js
@@ -2,14 +2,8 @@ const fs = require("fs");
 const path = require("path");
 
 const NGINX_CONF_FILEPATH = path.resolve("/etc/nginx/nginx.conf");
-const NGINX_MIME_TYPES_FILEPATH = path.resolve("/etc/nginx/mime.types");
 
-const mimeTypes = Array.from(
-  new Set(
-    fs.readFileSync(NGINX_MIME_TYPES_FILEPATH, "utf-8").match(/\S+\/\S+/gm)
-  )
-);
 const updatedNginxConf = fs
   .readFileSync(NGINX_CONF_FILEPATH, "utf-8")
-  .replace(/.*gzip_types.*/gm, `\tgzip_types ${mimeTypes.join(" ")};`);
+  .replace(/.*gzip_types.*/gm, "\tgzip_types *;");
 fs.writeFileSync(NGINX_CONF_FILEPATH, updatedNginxConf);
