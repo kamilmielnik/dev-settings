a571b000c4f3404b2afc336ae6143252e9521617
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-05-10 02:01:06 +0700

    Make sure mime-types are unique

--- vps/nginx/enable-gzip-types.js
+++ vps/nginx/enable-gzip-types.js
@@ -4,9 +4,11 @@ const path = require("path");
 const NGINX_CONF_FILEPATH = path.resolve("/etc/nginx/nginx.conf");
 const NGINX_MIME_TYPES_FILEPATH = path.resolve("/etc/nginx/mime.types");
 
-const mimeTypes = fs
-  .readFileSync(NGINX_MIME_TYPES_FILEPATH, "utf-8")
-  .match(/\S+\/\S+/gm);
+const mimeTypes = Array.from(
+  new Set(
+    fs.readFileSync(NGINX_MIME_TYPES_FILEPATH, "utf-8").match(/\S+\/\S+/gm)
+  )
+);
 const updatedNginxConf = fs
   .readFileSync(NGINX_CONF_FILEPATH, "utf-8")
   .replace(/.*gzip_types.*/gm, `\tgzip_types ${mimeTypes.join(" ")};`);
